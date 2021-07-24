774060adf2faa5f2263e469051019014676bb33a
Yoon Jinyeong <wwwz4805@kakao.com>
2021-07-24 17:00:04 +0900

    Update LazyVGridView

--- SwiftUI/SwiftUIPrototypingPlaygroundBookBaseCodes/SwiftUIPrototypingPlaygroundBookBaseCode/SwiftUIPrototypingPlaygroundBookBaseCode/ContentView.swift
+++ SwiftUI/SwiftUIPrototypingPlaygroundBookBaseCodes/SwiftUIPrototypingPlaygroundBookBaseCode/SwiftUIPrototypingPlaygroundBookBaseCode/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+struct GaugeProgressStyle: ProgressViewStyle {
+    var strokeColor = Color.blue
+    var strokeWidth = 25.0
+
+    func makeBody(configuration: Configuration) -> some View {
+        let fractionCompleted = configuration.fractionCompleted ?? 0
+
+        return ZStack {
+            
+            Circle()
+                .stroke(lineWidth: 20.0)
+                .opacity(0.3)
+                .foregroundColor(strokeColor)
+            
+            Circle()
+                .trim(from: 0, to: CGFloat(fractionCompleted))
+                .stroke(strokeColor, style: StrokeStyle(lineWidth: CGFloat(strokeWidth), lineCap: .round))
+                .rotationEffect(.degrees(-90))
+        }
+    }
+}
+
+
 struct ContentView: View {
+    @State private var progress = 0.2
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ProgressView(value: progress, total: 1.0)
+            .progressViewStyle(GaugeProgressStyle())
+            .frame(width: 200, height: 200)
     }
 }
 
---
+++ SwiftUI/SwiftUIPrototypingPlaygroundBookBaseCodes/SwiftUIPrototypingPlaygroundBookBaseCode/SwiftUIPrototypingPlaygroundBookBaseCode/LazyVGridView.swift
@@ -0,0 +1,62 @@
+//
+//  LazyVGridView.swift
+//  SwiftUIPrototypingPlaygroundBookBaseCode
+//
+//  Created by 윤진영 on 2021/07/22.
+//
+
+import SwiftUI
+
+struct Photo: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+let samplePhotos = (1...20).map { Photo(name: "coffee-\($0)") }
+
+struct LazyVGridView: View {
+    
+    @State var gridLayout: [GridItem] = [ GridItem() ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    
+                    ForEach(samplePhotos.indices) { index in
+                        
+                        Image(samplePhotos[index].name)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .frame(height: gridLayout.count == 1 ? 200 : 100)
+                            .cornerRadius(10)
+                            .shadow(color: Color.primary.opacity(0.3), radius: 1)
+                        
+                    }
+                }
+                .padding(.all, 10)
+                .animation(.interactiveSpring())
+            }
+            
+            .navigationTitle("Coffee Feed")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        self.gridLayout = Array(repeating: .init(.flexible()), count: self.gridLayout.count % 4 + 1)
+                    }) {
+                        Image(systemName: "square.grid.2x2")
+                            .font(.title)
+                            .foregroundColor(.primary)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct LazyVGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyVGridView()
+    }
+}
