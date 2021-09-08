5ae6ed1c58fca6073083702866817863e4fd347f
Yoon Jinyeong <wwwz4805@kakao.com>
2021-09-08 15:41:13 +0900

    [SwiftUI - AppleSwiftUITutorial] Create Projects

---
+++ SwiftUI/AppleSwiftUITutorial/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 윤진영 on 2021/09/08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/AppleSwiftUITutorial/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 윤진영 on 2021/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
