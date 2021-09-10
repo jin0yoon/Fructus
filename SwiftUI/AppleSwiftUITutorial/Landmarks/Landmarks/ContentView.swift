689ccf00a19adbba649dd590bf7f71a95ce17453
Yoon Jinyeong <wwwz4805@kakao.com>
2021-09-10 11:03:01 +0900

    [SwiftUI - AppleSwiftUITutorial] Update Landmarks

--- SwiftUI/AppleSwiftUITutorial/Landmarks/Landmarks/ContentView.swift
+++ SwiftUI/AppleSwiftUITutorial/Landmarks/Landmarks/ContentView.swift
@@ -9,12 +9,34 @@
  요소 -> Command + click -> inspector
  */
 
+/*
+ To customize a SwiftUI view, you call methods called modifiers.
+ Modifiers wrap a view to change its display or other properties.
+ Each modifier returns a new view,
+ so it’s common to chain multiple modifiers, stacked vertically.
+ */
+
+/*
+ A spacer expands to make its containing view use all of the space of its parent view, instead of having its size defined only by its contents.
+ */
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
