0a3535fdec6d07dc30c3d1a179f24c3f84baa126
Yoon Jinyeong <wwwz4805@kakao.com>
2021-09-13 12:11:31 +0900

    [AppleGuideProjects/ARkit_Template] Create AR Project

---
+++ UikitProjects/AppleGuideProjects/ARkit_Template/ARkit_Template/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARkit_Template
+//
+//  Created by 윤진영 on 2021/09/13.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
